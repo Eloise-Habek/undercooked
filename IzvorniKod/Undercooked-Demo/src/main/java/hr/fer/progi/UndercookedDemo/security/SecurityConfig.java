b32e9a11068ddcdeff7a74aa144a1cc0e8ba925c
AlanDurdevic <alan.durdevic@fer.hr>
2023-11-09 13:20:21 +0100

    Riješen bug sa h2-console te dodan logout

--- IzvorniKod/Undercooked-Demo/src/main/java/hr/fer/progi/UndercookedDemo/security/SecurityConfig.java
+++ IzvorniKod/Undercooked-Demo/src/main/java/hr/fer/progi/UndercookedDemo/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -12,17 +13,17 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable())
-                .authorizeRequests(auth -> {
-                    auth.antMatchers("/", "/login", "/register").permitAll();
-                    auth.antMatchers("/profile").hasRole("USER");
-                    auth.antMatchers("/persons").hasRole("ADMIN"); 
-                })
-                .httpBasic(withDefaults())
-                .build();
-    }
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable());
+		http.headers().frameOptions().disable();
+		http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
+				.deleteCookies("JSESSIONID").clearAuthentication(true).invalidateHttpSession(true);
+		return http.authorizeRequests(auth -> {
+			auth.antMatchers("/", "/login", "/register").permitAll();
+			auth.antMatchers("/profile").hasRole("USER");
+			auth.antMatchers("/persons/**", "/h2-console/**").hasRole("ADMIN");
+		}).httpBasic(withDefaults()).build();
+	}
 
 }
