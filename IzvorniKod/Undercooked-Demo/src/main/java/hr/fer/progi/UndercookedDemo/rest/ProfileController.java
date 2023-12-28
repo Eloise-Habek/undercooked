0db02fbc46fcb5cd4da9d365211175dec7f879d6
AlanDurdevic <alan.durdevic@fer.hr>
2023-12-28 10:12:19 +0100

    Popravljen ProfileController (za sada)

--- IzvorniKod/Undercooked-Demo/src/main/java/hr/fer/progi/UndercookedDemo/rest/ProfileController.java
+++ IzvorniKod/Undercooked-Demo/src/main/java/hr/fer/progi/UndercookedDemo/rest/ProfileController.java
@@ -1,10 +1,9 @@
 package hr.fer.progi.UndercookedDemo.rest;
 
+import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +21,16 @@ public class ProfileController {
 	
 	//treba popraviti
 	@GetMapping("")
-	public Person myProfile(@AuthenticationPrincipal User user) {
-		Optional<Person> person = personService.findByUsername(user.getUsername());
+	public Person myProfile(Principal principal) {
+		Optional<Person> person = personService.findByUsername(principal.getName());
+		if(principal.getName().equals("admin")) {
+			Person p = new Person();
+			p.setUsername("admin");
+			p.setEmail("undercooked@fer.hr");
+			p.setName("admin");
+			p.setEmail("admin");
+			return p;
+		}
 		return person.get();
 	}
 
