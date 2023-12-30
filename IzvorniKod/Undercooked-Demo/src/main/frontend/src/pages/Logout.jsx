5b9a7c6badfe9212addf1883841fa16a8224d18e
Mateo Plavec <mateo.plavec@fer.hr>
2023-12-30 23:10:12 +0100

    Izbriši random frontend folder
    Pravi frontend se nalazu u ../undercooked-frontent

--- IzvorniKod/Undercooked-Demo/src/main/frontend/src/pages/Logout.jsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "react-router-dom";
-import secureLocalStorage from "react-secure-storage";
-
-export function Logout() {
-    secureLocalStorage.removeItem("logInToken")
-    //window.location.reload(false);
-    return <h1>Logged out</h1>
-}
