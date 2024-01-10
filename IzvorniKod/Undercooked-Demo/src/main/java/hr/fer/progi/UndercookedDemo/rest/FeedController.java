6ebe8a0aec06f40af62c979c7a524aaf8ff38672
AlanDurdevic <alan.durdevic@fer.hr>
2024-01-10 16:28:59 +0100

    Dodana ruta za feed od pracenih korisnika

---
+++ IzvorniKod/Undercooked-Demo/src/main/java/hr/fer/progi/UndercookedDemo/rest/FeedController.java
@@ -0,0 +1,34 @@
+package hr.fer.progi.UndercookedDemo.rest;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hr.fer.progi.UndercookedDemo.domain.Person;
+import hr.fer.progi.UndercookedDemo.domain.Recipe;
+import hr.fer.progi.UndercookedDemo.service.FollowersService;
+
+@RestController
+@RequestMapping("/feed")
+public class FeedController {
+	
+	@Autowired
+	private FollowersService followersService;
+	
+	@GetMapping("/recipes")
+	public List<Recipe> getFollowingRecipes(Principal principal){
+		List<Person> following = followersService.getFollowing(principal.getName());
+		List<Recipe> recipes = new ArrayList<>();
+		for(Person p : following) {
+			recipes.addAll(p.getRecipes());
+		}
+		return recipes;
+	}
+	
+
+}
