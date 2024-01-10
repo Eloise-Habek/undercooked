a95e4721e04b380bf88de10aac4a8d3340025d61
Matej Magat <matej.magat@fer.hr>
2024-01-10 17:19:26 +0100

    Implementiran feed (Frontend)

--- IzvorniKod/undercooked-frontend/src/pages/Home.jsx
+++ IzvorniKod/undercooked-frontend/src/pages/Home.jsx
@@ -1,67 +1,24 @@
-import { PageNav } from "../components/PageNav";
 import { RecipeMini } from "../components/RecipeMini";
 import { Footer } from "./wrapper/Footer";
 import classes from "../styles/home/home.module.css";
+import { useEffect, useMemo, useState } from "react";
+import FeedService from "../services/FeedService";
 
 export function Home() {
-    const temp = {
-        "id": 1,
-        "name": "kruh",
-        "preparationTime": "PT1H30M",
-        "description": "neobičan bijeli kruh",
-        "preparationDescription": "1. Pomješaj sastojke\n2. Stavi u pećnicu na 180°C i 90 minuta\n3. Uživaj!",
-        "author": {
-            "id": 3,
-            "username": "matej",
-            "name": "matej",
-            "surname": "magat",
-            "isAdmin": false
-        },
-        "ingredients": [
-            {
-                "amount": 250.0,
-                "unitOfMeasure": "g",
-                "ingredient": {
-                    "id": 1,
-                    "name": "glatkog brasna"
-                }
-            },
-            {
-                "amount": 2.0,
-                "unitOfMeasure": null,
-                "ingredient": {
-                    "id": 2,
-                    "name": "jAJa"
-                }
-            },
-            {
-                "amount": null,
-                "unitOfMeasure": "žlica",
-                "ingredient": {
-                    "id": 3,
-                    "name": "VOde"
-                }
-            }
-        ]
-    }
+    const feedService = useMemo(() => new FeedService(), [])
+    const [recipes, setRecipes] = useState([]);
+    useEffect(() => {
+        feedService.getRecipes().then((data) => {
+            setRecipes(data.map((e) => {
+                return <li key={e.id}><RecipeMini details={e} /></li>
+            }));
+        }, () => { })
+    }, [feedService])
     return (
         <>
-            {/* <div className="main_div">
-        <h1 className="naslov">Dobrodošli na CookBooked!</h1>
-
-        <div className="search-container">
-            <input type="text" className="search-input" placeholder="Pretraži recepte..."/>
-            <button className="search-button">Pretraži</button>
-        </div>
-    </div> */}
-            <div className={classes.wrapper}>
-                <RecipeMini details={temp} />
-                <RecipeMini details={temp} />
-                <RecipeMini details={temp} />
-                <RecipeMini details={temp} />
-                <RecipeMini details={temp} />
-                <RecipeMini details={temp} />
-            </div>
+            <ul className={classes.wrapper}>
+                {recipes.length > 0 ? recipes : "Follow people to see their posts"}
+            </ul>
             {/* <PageNav /> */}
             <Footer sticky={1} />
         </>
